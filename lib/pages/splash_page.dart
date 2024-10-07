9b8291759efc461974cb552fbd8208f4c811477a
cukiprit <andhikadwikyp@gmail.com>
2024-10-08 05:16:02 +0700

    Fully Responsive

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -9,48 +9,50 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(20.0),
-              child: const Text(
-                "UNLOCK YOUR FINANCIAL FUTURE.",
-                textAlign: TextAlign.justify,
-                style: TextStyle(
-                  fontSize: 40,
-                  fontVariations: [
-                    FontVariation('wght', 800),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SvgPicture.asset(
-                'assets/images/piggy_bank.svg',
-                width: 300, // You can adjust the size
-                height: 300,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => const HomePage(),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(20.0),
+                child: const Text(
+                  "UNLOCK YOUR FINANCIAL FUTURE.",
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontVariations: [
+                      FontVariation('wght', 800),
+                    ],
                   ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
                 ),
               ),
-              child: const Text('Get Started'),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset(
+                  'assets/images/piggy_bank.svg',
+                  width: 300, // You can adjust the size
+                  height: 300,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => const HomePage(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+                child: const Text('Get Started'),
+              ),
+            ],
+          ),
         ),
       ),
     );
